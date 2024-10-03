bd44396b3ca644d223e4e6db8833f6d83cbc7607
favtuts <favtuts@gmail.com>
2024-10-03 11:09:02 +0700

    decode JSON to maps unstructured data

---
+++ json-examples/json_to_maps_unstructured_data.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	birdJson := `{"birds":{"pigeon":"likes to perch on rocks","eagle":"bird of prey"},"animals":"none"}`
+	var result map[string]any
+	json.Unmarshal([]byte(birdJson), &result)
+
+	// The object stored in the "birds" key is also stored as
+	// a map[string]any type, and its type is asserted from
+	// the `any` type
+	birds := result["birds"].(map[string]any)
+
+	for key, value := range birds {
+		// Each value is an `any` type, that is type asserted as a string
+		fmt.Println(key, value.(string))
+	}
+}
