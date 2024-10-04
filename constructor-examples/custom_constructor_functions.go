4af082a1e991d2919c1ddfc9b6243f3058bb3eea
favtuts <favtuts@gmail.com>
2024-10-04 14:31:46 +0700

    demo constructor returning errors

--- constructor-examples/custom_constructor_functions.go
+++ constructor-examples/custom_constructor_functions.go
@@ -17,6 +17,26 @@ func NewPizza(toppings []string) Pizza {
 	}
 }
 
+func getToppings(url string) ([]string, error) {
+	// this should fetch some toppings from some remote URL
+	// return []string{"peppers"}, nil
+	return nil, fmt.Errorf("cannot fetch Piza info from url: %v", url)
+}
+
+func NewRemotePizza(url string) (Pizza, error) {
+	// toppings are received from a remote URL, which may fail
+	toppings, err := getToppings(url)
+	if err != nil {
+		// if an error occurs, return the wrapped error along with an empty
+		// Pizza instance
+		return Pizza{}, fmt.Errorf("could not construct new Pizza: %v", err)
+	}
+	return Pizza{
+		slices:   6,
+		toppings: toppings,
+	}, nil
+}
+
 func main() {
 
 	fmt.Println(NewPizza(nil)) // {6 []}
@@ -26,4 +46,11 @@ func main() {
 
 	otherPizza := NewPizza([]string{"onion", "pineapple"})
 	fmt.Println(otherPizza) // {6 [onion pineapple]}
+
+	remotePizza, err := NewRemotePizza("http://mypizza.org")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(remotePizza)
+	}
 }
